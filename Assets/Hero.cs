70dbfe58f6bd9a6df1f8c87a28f19c3a44454ff1
JosephRutgers42 <55005555+JosephRutgers42@users.noreply.github.com>
2019-10-30 23:38:34 -0400

    Game Restarts and Everything is Working Correctly

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -12,10 +12,12 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public float gameRestartDelay = 2f;
 
     [Header("Set Dynamically")]
+    [SerializeField]
 
-    public float shieldLevel = 1;
+    private float _shieldLevel = 1;
     private GameObject lastTriggerGo = null;
 
     void Awake() {
@@ -63,5 +65,17 @@ public class Hero : MonoBehaviour
             print("Triggered by non-Enemy: " + go.name);
         }
         print("Triggered: " + go.name);
+    }
+    public float shieldLevel {
+        get {
+            return (_shieldLevel);
+        }
+        set {
+            _shieldLevel = Mathf.Min(value, 4);
+            if (value < 0) {
+                Destroy(this.gameObject);
+                Main.S.DelayedRestart(gameRestartDelay);
+            }
+        }
     }
 }
--- Assets/Main.cs
+++ Assets/Main.cs
@@ -36,6 +36,12 @@ public class Main : MonoBehaviour {
         Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
 
     }
+    public void DelayedRestart(float delay) {
+        Invoke("Restart", delay);
+    }
+    public void Restart() {
+        SceneManager.LoadScene("SpaceShooterFinal");
+    }
 
 
     // Start is called before the first frame update
